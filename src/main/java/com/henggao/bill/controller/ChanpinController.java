5a809fe793a7da2114afed1c379f910e8462c512
songsm <18994656675@189.cn>
2019-03-31 22:43:36 +0800

    Initial commit

---
+++ src/main/java/com/henggao/bill/controller/ChanpinController.java
@@ -0,0 +1,23 @@
+package com.henggao.bill.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author songsm
+ * @data 2019/3/31 下午10:10
+ * @desc 用一句话描述此类的作用
+ */
+
+@RestController
+public class ChanpinController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+
+        return "hello";
+    }
+}
